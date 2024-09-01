d0eed1afca1234c20012716ea140f1ebb444dbfe
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-09-01 11:26:04 +0630

    Now using vite

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './assets/css/tailwind.css'; // Import Tailwind CSS
+import Toast, { POSITION } from 'vue-toastification';
+import "vue-toastification/dist/index.css";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Toast, {
+    position: POSITION.TOP_CENTER,
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: false,
+    closeButton: 'button',
+    icon: true,
+    rtl: false,
+}).mount('#app')
