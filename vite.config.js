ca87e7a8da8063febba1496b1c29185ed994273a
Phyo Kyaw San <phyojupiter9@gmail.com>
2024-09-13 10:57:56 +0630

    Service worker setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,65 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [vue()],
+    base: '/',
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            devOptions: {
+                enabled: true,
+                type: 'module'
+            },
+            workbox: {
+                swDest: 'dist/service-worker.js',
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'google-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: ({ request }) => request.destination === 'image',
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'image-cache',
+                            expiration: {
+                                maxEntries: 50,
+                                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+                            },
+                        },
+                    },
+                ],
+            },
+            manifest: {
+                name: 'Cloudy Weather',
+                short_name: 'CloudyWeather',
+                description: 'Weather app to provide location based weather info and forecast',
+                theme_color: '#0000BF',
+                icons: [
+                    {
+                        src: 'img/icons/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'img/icons/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        })
+    ],
     resolve: {
         alias: {
             '@': '/src'
